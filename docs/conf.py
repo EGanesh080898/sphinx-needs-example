9a669a27306c7a87c524a88e464b8be12eb8c3bf
Ganesh Ettigadda <ganeshrockz08@gmail.com>
2025-08-14 04:43:26 +0000

    Added requirement REQ_001 for math addition

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Sphinx-Needs Demo'
+project = 'Math Project'
 author = 'Ganesh'
 release = '1.0'
 
@@ -10,7 +10,14 @@ extensions = [
     'sphinx_needs',
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
+needs_types = [
+    dict(directive='req', title='Requirement', prefix='REQ_'),
+    dict(directive='spec', title='Specification', prefix='SPEC_'),
+    dict(directive='test', title='Test Case', prefix='TEST_'),
+]
+
+needs_extra_options = [
+    'author'
+]
 
 html_theme = 'alabaster'
